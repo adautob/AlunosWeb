fec0943dd52d277fd6a08a38bda27d4eb2cb667d
Adauto Bueno <adauto.bueno@gmail.com>
2022-10-04 00:42:14 -0300

    Spring adicionado ao projeto

---
+++ src/main/java/com/csv/AlunosWeb/AlunosWebApplication.java
@@ -0,0 +1,13 @@
+package com.csv.AlunosWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunosWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csv/AlunosWeb/AlunosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csv.AlunosWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
