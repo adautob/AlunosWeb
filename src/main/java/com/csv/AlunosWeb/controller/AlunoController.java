d2eeb65f136b03e17ba86655a650f954dad0bdbb
Adauto Bueno <adauto.bueno@gmail.com>
2022-10-10 00:52:02 -0300

    create.html criado

--- src/main/java/com/csv/AlunosWeb/controller/AlunoController.java
+++ src/main/java/com/csv/AlunosWeb/controller/AlunoController.java
@@ -2,31 +2,35 @@ package com.csv.AlunosWeb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.csv.AlunosWeb.dao.AlunoDao;
+import com.csv.AlunosWeb.model.Aluno;
 
 @Controller
 public class AlunoController {
 
-	@RequestMapping("alunos")
+	@GetMapping("alunos")
 	public String listaAlunos(ModelMap modelMap) {
 		AlunoDao alunoDao = new AlunoDao();
 		modelMap.addAttribute("lista", alunoDao.SelecionarTodos());
-				
+
 		return "alunos";
 	}
-	
-	@RequestMapping("alunos/novo")
-	public String novoAluno() {
-		return "novoAluno";
+
+	@GetMapping("alunos/create")
+	public ModelAndView novoAluno() {
+	    ModelAndView mv = new ModelAndView("create");
+	    mv.addObject("aluno", new Aluno());
+	    return mv;
 	}
-	
-	@RequestMapping("alunos/salvar")
+
+	@PostMapping("alunos/create")
 	public String salvarAluno(ModelMap modelMap) {
-		
+
 		return "alunos";
 	}
-	
-	
+
 }
--- src/main/java/com/csv/AlunosWeb/controller/HomeController.java
+++ src/main/java/com/csv/AlunosWeb/controller/HomeController.java
@@ -1,11 +1,11 @@
 package com.csv.AlunosWeb.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {	
-	@RequestMapping("home")
+	@GetMapping("home")
 	public String index() {
 		return "index";
 	}
