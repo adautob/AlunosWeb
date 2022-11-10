9211ce8275e630e79489a0696192b2cf90ff7466
Adauto Bueno <adauto.bueno@gmail.com>
2022-11-09 23:35:24 -0300

    atualizado documentação do código

--- src/main/java/com/csv/AlunosWeb/dao/AlunoDao.java
+++ src/main/java/com/csv/AlunosWeb/dao/AlunoDao.java
@@ -27,8 +27,11 @@ public class AlunoDao {
 		
 	}
 	
+	// percorre a lista de alunos para remover o aluno
 	public void RemoverAluno(int id) {
 		for (int i = 0; i < Banco.alunos.size(); i++) {
+			// compara se o Id recebido no parâmetro é o mesmo que está sendo iterado
+			// se for igual remove o aluno da lista
 			if (Banco.alunos.get(i).getId()==id)
 				Banco.alunos.remove(i);
 		}
@@ -40,6 +43,7 @@ public class AlunoDao {
 		return Banco.alunos;	
 	}
 	
+	// percorre a lista para encontrar um aluno pelo id
 	public Aluno getAluno(int index) {
 		Aluno aluno = new Aluno();
 		for (int i = 0; i < Banco.alunos.size(); i++) {
@@ -49,6 +53,7 @@ public class AlunoDao {
 		return aluno;
 	}
 	
+	// atualiza o aluno na lista, recebendo como parâmetro o id a ser alterado e o Aluno
 	public void AtualizarAluno(int id, Aluno aluno) {
 		for (int i = 0; i < Banco.alunos.size(); i++) {
 			if (Banco.alunos.get(i).getId()==id)
